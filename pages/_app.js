448c6974cfe93d7a0cef9c477bd17046987e6b7e
iko.lin <chiwei991814@gmail.com>
2021-11-07 08:51:51 +0800

    feat: add material ui and reset.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import "../styles/globals.css";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
+import "styles/reset.css";
+import theme from "configs/theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
