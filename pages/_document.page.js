3fcccb336b6f5708bae5c009c40d5eada62b8b6f
WEI.LIN <chiwei991814@gmail.com>
2021-12-05 11:29:03 +0800

    feat: details page layout

--- pages/_document.page.js
+++ pages/_document.page.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
             crossOrigin="true"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@700&family=Noto+Serif+TC:wght@500&family=Roboto:wght@300;400;500;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@700&family=Noto+Serif+TC:wght@500;700&family=Roboto:wght@300;400;500;700&display=swap"
             rel="stylesheet"
           />
         </Head>
