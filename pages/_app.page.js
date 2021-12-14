aa83d3bccc68a1ddcf52f6c5acf903aa67c71924
WEI.LIN <chiwei991814@gmail.com>
2021-12-14 08:09:03 +0800

    fix: fix Material-UI SSR flickering issues

--- pages/_app.page.js
+++ pages/_app.page.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import { SnackbarProvider } from "notistack";
 
@@ -6,6 +7,13 @@ import { AppProvider } from "contexts/AppContext";
 import theme from "configs/theme";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
