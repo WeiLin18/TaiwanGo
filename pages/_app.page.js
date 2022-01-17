b2df1ba2bdfffcbb6e98ac579702cf12fbb40807
WEI.LIN <chiwei991814@gmail.com>
2022-01-17 10:33:32 +0800

    fix: adjust theme injection

--- pages/_app.page.js
+++ pages/_app.page.js
@@ -7,9 +7,16 @@ import "styles/reset.css";
 import { AppProvider } from "contexts/AppContext";
 import themeConfig from "configs/theme";
 
-function MyApp({ Component, pageProps }) {
-  const theme = createTheme(themeConfig);
+import { CacheProvider } from "@emotion/react";
+import createEmotionCache from "utils/createEmotionCache";
 
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  const theme = createTheme(themeConfig);
   useEffect(() => {
     const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
@@ -19,25 +26,27 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Head>
-        <title>TaiwanGo</title>
-        <meta name="description" content="Taiwan travel information" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <SnackbarProvider
-          maxSnack={3}
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "left",
-          }}
-        >
-          <AppProvider>
-            <Component {...pageProps} />
-          </AppProvider>
-        </SnackbarProvider>
-      </ThemeProvider>
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <title>TaiwanGo</title>
+          <meta name="description" content="Taiwan travel information" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <SnackbarProvider
+            maxSnack={3}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+          >
+            <AppProvider>
+              <Component {...pageProps} />;
+            </AppProvider>
+          </SnackbarProvider>
+        </ThemeProvider>
+      </CacheProvider>
     </>
   );
 }
